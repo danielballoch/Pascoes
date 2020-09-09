1d6d6e1a15a0bf1a4f0eda16fda76c79b797093a
Daniel Balloch <43333774+danielpbdesign@users.noreply.github.com>
2020-09-09 12:41:18 +1200

    setup prismic cms and got header text loading dynamically in contact page w/ propTypes check

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,50 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, {Component} from "react"
+import { graphql, Link } from "gatsby"
+import PropTypes from 'prop-types'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Contact</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+class Contact extends Component {
+    
+    render(){
+        const {
+            data: {contact},
+        } = this.props
+        console.log(contact)
+        return(
+            <Layout>
+            <SEO title="Page two" />
+            <h1>{contact.data.header.text}</h1>
+            <p>Welcome to page 2</p>
+            <Link to="/">Go back to the homepage</Link>
+            </Layout>
+        )
+    }
+} 
+export default Contact
 
-export default SecondPage
+Contact.propTypes = {
+    data: PropTypes.shape({
+        contact: PropTypes.shape({
+            data: PropTypes.shape({
+                header: PropTypes.shape({
+                    text: PropTypes.string.isRequired
+                })
+            })
+        })
+    }).isRequired,
+}
+    
+
+export const pageQuery = graphql`
+query IndexQuery {
+    contact: prismicContact {
+        data {
+            header {
+                text
+            }
+        }
+    }
+}
+`
